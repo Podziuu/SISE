21b49623fa2908258e4183f5822350e9b4507c46
Bartosz  Podemski <bartekpodemski@gmail.com>
2024-03-21 19:16:47 +0100

    bfs

--- zadanie1/bfs.py
+++ zadanie1/bfs.py
@@ -1,25 +1,29 @@
 from collections import deque
 from node import Node
 
-def bfs(start_state):
+def bfs(start_state, order):
     start_node = Node(start_state)
     if start_node.state == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]:
         return []
+    
+    print(order)
 
     visited = set()
+    visited.add(tuple(start_node.state))
     queue = deque([start_node])
     
     while queue:
         node = queue.popleft()
-        visited.add(tuple(node.state))
-        
-        for neighbor in node.get_neighbors():
-            if tuple(neighbor.state) not in visited:
-                if neighbor.state == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]:
-                    path = []
-                    while neighbor.parent:
-                        path.append(neighbor.action)
-                        neighbor = neighbor.parent
-                    path.reverse()
-                    return path
-                queue.append(neighbor)
+        for direction in order:
+            for neighbor in node.get_neighbors(direction):
+                neighbor_state_tuple = tuple(neighbor.state)
+                if neighbor_state_tuple not in visited:
+                    visited.add(neighbor_state_tuple)
+                    if neighbor.state == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]:
+                        path = []
+                        while neighbor.parent:
+                            path.append(neighbor.action)
+                            neighbor = neighbor.parent
+                        path.reverse()
+                        return path
+                    queue.append(neighbor)
--- zadanie1/node.py
+++ zadanie1/node.py
@@ -4,22 +4,22 @@ class Node:
         self.parent = parent # rodzic węzła
         self.action = action # ruch, którym rodzic przeszedł do tego węzła
         
-    def get_neighbors(self):
+    def get_neighbors(self, direction):
         neighbors = []
         empty = self.state.index(0)
-        if empty % 4 != 0: # ruch w lewo
+        if empty % 4 != 0 and direction == "L": # ruch w lewo
             new_state = self.state[:]
             new_state[empty], new_state[empty - 1] = new_state[empty - 1], new_state[empty]
             neighbors.append(Node(new_state, self, "L"))
-        if empty % 4 != 3: # ruch w prawo
+        if empty % 4 != 3 and direction == "R": # ruch w prawo
             new_state = self.state[:]
             new_state[empty], new_state[empty + 1] = new_state[empty + 1], new_state[empty]
             neighbors.append(Node(new_state, self, "R"))
-        if empty > 3: # ruch w góre
+        if empty > 3 and direction == "U": # ruch w góre
             new_state = self.state[:]
             new_state[empty], new_state[empty - 4] = new_state[empty - 4], new_state[empty]
             neighbors.append(Node(new_state, self, "U"))
-        if empty < 12: # ruch w dół
+        if empty < 12 and direction == "D": # ruch w dół
             new_state = self.state[:]
             new_state[empty], new_state[empty + 4] = new_state[empty + 4], new_state[empty]
             neighbors.append(Node(new_state, self, "D"))
