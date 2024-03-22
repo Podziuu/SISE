3478d0a064e3de3603ca6e9c91d50441e80a044c
Wojtyla23 <247734@edu.p.lodz.pl>
2024-03-22 11:37:51 +0100

    Implementacja DFS i poprawki BFS

--- zadanie1/bfs.py
+++ zadanie1/bfs.py
@@ -14,16 +14,24 @@ def bfs(start_state, order):
     
     while queue:
         node = queue.popleft()
+        # print(node.state[:4])
+        # print(node.state[4:8])
+        # print(node.state[8:12])
+        # print(node.state[12:16])
+        # print()
+
+        if node.state == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]:
+            path = []
+            while node.parent:
+                path.append(node.action)
+                node = node.parent
+            path.reverse()
+            return path
+
         for direction in order:
-            for neighbor in node.get_neighbors(direction):
+            neighbor = node.get_neighbors(direction)
+            if neighbor is not None:
                 neighbor_state_tuple = tuple(neighbor.state)
                 if neighbor_state_tuple not in visited:
                     visited.add(neighbor_state_tuple)
-                    if neighbor.state == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]:
-                        path = []
-                        while neighbor.parent:
-                            path.append(neighbor.action)
-                            neighbor = neighbor.parent
-                        path.reverse()
-                        return path
                     queue.append(neighbor)
--- zadanie1/main.py
+++ zadanie1/main.py
@@ -1,9 +1,19 @@
 from bfs import bfs
+from dfs import dfs
 from astar import astar
 # [13,15,7,1,8,2,4,11,10,3,12,9,14,6,5,0]
 #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 0, 14, 15]
-start_state = [1, 6, 2, 4, 0, 5, 3, 8, 9, 10, 7, 11, 13, 14, 15, 12]
-solution = bfs(start_state, "LURD")
-solution2 = astar(start_state, "hamm")
+#[1, 0, 2, 4, 5, 7, 3, 8, 9, 6, 11, 12, 13, 10, 14, 15]
+order = "LURD"
+#start_state = [1, 0, 2, 4, 5, 7, 3, 8, 9, 6, 11, 12, 13, 10, 14, 15]
+#start_state = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 11, 13, 14, 15, 12]
+start_state = [1, 2, 3, 0,
+5, 7, 8, 4,
+9, 6, 10, 12,
+13, 14, 11, 15]
+solution = bfs(start_state, order)
+solution2 = dfs(start_state, order)
+#solution3 = astar(start_state, "hamm")
 print("Rozwiązanie:", solution)
-print("Rozwiązanie2:", solution2)
+print("Rozwiazanie DFS", solution2)
+#print("Rozwiązanie2:", solution3)
