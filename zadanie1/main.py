2df5142c2e81c9f62e328593f46afcb6fc204f9d
Bartosz  Podemski <bartekpodemski@gmail.com>
2024-03-26 17:28:12 +0100

    Add sys arguments

--- zadanie1/main.py
+++ zadanie1/main.py
@@ -1,9 +1,16 @@
 from bfs import bfs
 from dfs import dfs
 from astar import astar
+import sys
+
 # [13,15,7,1,8,2,4,11,10,3,12,9,14,6,5,0]
 #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 0, 14, 15]
 # [1, 2, 3, 4, 5, 6, 11, 7, 9, 10, 0, 8, 13, 14, 15, 12]
+strategy = sys.argv[1]
+additional_parameter = sys.argv[2]
+input_filename = sys.argv[3]
+output_filename = sys.argv[4]
+additional_output_filename = sys.argv[5]
 start_state = [1, 2, 3, 4, 5, 6, 11, 7, 9, 10, 0, 8, 13, 14, 15, 12]
 solution = bfs(start_state, "LURD")
 solution2 = astar(start_state, "hamm")
