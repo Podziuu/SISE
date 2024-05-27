b51c903510343c4f5fd16f4707da4f7530c4b83f
Bartosz Podemski <bartekpodemski@gmail.com>
2024-05-27 16:05:18 +0200

    fetch Data script

---
+++ zadanie3/fetchData.py
@@ -0,0 +1,18 @@
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+mushroom = fetch_ucirepo(id=73) 
+  
+# data (as pandas dataframes) 
+X = mushroom.data.features 
+X.to_csv('zadanie3/data/features.csv')
+y = mushroom.data.targets 
+y.to_csv('zadanie3/data/targets.csv')
+  
+# metadata 
+print(mushroom.metadata) 
+  
+# variable information 
+variableInformation = mushroom.variables
+print(variableInformation)
+variableInformation.to_csv('zadanie3/data/variable_information.csv') 
